8afe84c0334a3d46908807e02a5b2ac6a97a7732
Vinicius Soares <vsoares446@gmail.com>
2020-10-28 16:28:36 -0300

    Desfazendo alteracao no codigo do site.

--- src/template/template.ino
+++ src/template/template.ino
@@ -1,27 +1,9 @@
-#define lamp 4
-#define ldr A0
-int valor = 0;
-int interruptor = 0;
+void setup() {
+  // put your setup code here, to run once:
 
-void setup()
-{
-  Serial.begin(9600);
-  pinMode(lamp, OUTPUT);
-  pinMode(ldr, INPUT);
-  pinMode(6,INPUT);
 }
 
-void loop()
-{
-  valor = analogRead(ldr);
-  interruptor=digitalRead(6);
-  if(valor > 350 && interruptor == LOW)
-  {
-  	digitalWrite(lamp,HIGH);
-  }
-  if(valor <= 350)
-  {
-  	digitalWrite(lamp,LOW);
-  }
-  Serial.println(valor);
+void loop() {
+  // put your main code here, to run repeatedly:
+
 }
